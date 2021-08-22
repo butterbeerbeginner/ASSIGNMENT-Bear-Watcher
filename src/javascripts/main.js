1dc01577558e6fe36eb129fcb76660b04f33de41
unknown <meredith.marcum@hotmail.com>
2021-08-22 18:39:18 -0500

    have displayed form, working on submit button

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,8 +1,40 @@
 import '../styles/main.scss';
 
+const bearInputForm = document.querySelector('#inputForm');
+
+// display form
+const inputForm = () => {
+  bearInputForm.innerHTML = `
+<div>
+    <form id="bearInputFormForm">
+      <div class="row g-3">
+      <div class="col">
+      <input type="text" class="form-control" placeholder="Bear Name" aria-label="Bear name">
+      </div>
+     <div class="col">
+      <input type="url" class="form-control" placeholder="Bear Image url" aria-label="Image UR">
+      </div>
+      </div>
+     <button type="submit" class="btn btn-primary" id="Submit">Submit</button>
+    </form>
+  </div>`;
+};
+
+const handleButtonClick = (event) => {
+  if (event.target.id === 'Submit') {
+    console.log('Submit Button Clicked');
+  }
+};
+
+const buttonEvents = () => {
+  bearInputForm.addEventListener('click', handleButtonClick);
+};
+
 const init = () => {
-  $('#app').html('<h1>HELLO! You are up and running!</h1>');
-  console.log('YOU ARE UP AND RUNNING!');
+  $('#app').html('<h1>Bear-Watcher</h1>');
+  inputForm();
+  buttonEvents();
+  handleButtonClick();
 };
 
 init();
